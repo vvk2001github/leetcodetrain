341b40d4290e7e0a102805160041669efba704c9
kulyaginv <kulyaginv@gmail.com>
2019-11-15 16:00:14 +0300

    20191115-1600

---
+++ myAtoi/myAtoi.py
@@ -0,0 +1,30 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.lstrip()
+        if s == '' or s == '-' or s == '+':
+            return 0
+        if((not (s[0] == '-' or s[0] == '+')) and (not s[0].isdigit())):
+            return 0
+        res_str:str = ''
+        end:int = 0
+        if(s[0] == '-' or s[0] == '+'):
+            end = 1
+        for i in range(end, len(s)):
+            if(not s[i].isdigit()):
+                end = i
+                break
+            else:
+                end = i
+        if not s[end].isdigit():
+            end -= 1
+        res_str = s[0:end+1]
+        res = int(res_str)
+        if(res < -2147483648):
+            res = -2147483648
+        if(res > 2147483647):
+            res = 2147483647
+        return res
+
+if __name__=='__main__':
+    sol: Solution = Solution()
+    print(sol.myAtoi('+1'))
