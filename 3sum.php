69aeeb45a6232609b763f078b07020faa2a4af20
kulyaginv <kulyaginv@gmail.com>
2022-07-26 15:59:51 +0300

    3sum.php

--- 3sum.php
+++ 3sum.php
@@ -8,34 +8,22 @@ class Solution {
     function threeSum($nums) {
         $arrayLength = count($nums);
         $result = [];
+        sort($nums);
 
-        for ( $i = 0; $i<$arrayLength; $i++ ) {
+        for ( $i = 0; $i<$arrayLength - 2; $i++ ) {
 
-            for ( $j = $i+1; $j<$arrayLength; $j++ ) {
+            for ( $j = $i+1; $j<$arrayLength - 1; $j++ ) {
 
-                if( $i == $j) continue;
                 
                 for ( $k = $j + 1; $k<$arrayLength; $k++ ) {
-                    if(($i == $k)or($j == $k)) continue;
 
                     if( ($nums[$i] + $nums[$j] + $nums[$k]) == 0) {
                         $tmp_result = [];
                         $tmp_result[] = $nums[$i];
                         $tmp_result[] = $nums[$j];
                         $tmp_result[] = $nums[$k];
-                        
-                        sort($tmp_result);
 
-                        $flag = false;
-
-                        foreach($result as $res) {
-                            if($res == $tmp_result) {
-                                $flag = true;
-                                break;
-                            }
-                        }
-
-                        if(!$flag) $result[] = $tmp_result;
+                        if(!in_array($tmp_result, $result)) $result[] = $tmp_result;
 
                     }
                 }
