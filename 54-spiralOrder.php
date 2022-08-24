2f65e2a0361438d4d215c5b946bb8167a88659a9
kulyaginv <kulyaginv@gmail.com>
2022-08-24 10:09:34 +0300

    20220824 109

---
+++ 54-spiralOrder.php
@@ -0,0 +1,22 @@
+<?php
+
+// 54 
+
+// Given an m x n matrix, return all elements of the matrix in spiral order.
+
+class Solution {
+
+    /**
+     * @param Integer[][] $matrix
+     * @return Integer[]
+     */
+    function spiralOrder($matrix) {
+        
+    }
+}
+
+$matrix = [[1,2,3],[4,5,6],[7,8,9]];
+
+$solution = new Solution();
+
+print_r( $solution->spiralOrder($matrix), false);
