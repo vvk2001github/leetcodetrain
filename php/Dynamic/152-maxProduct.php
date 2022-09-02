5ec93a240835f124d3517ba44d01536ac42b92ed
kulyaginv <kulyaginv@gmail.com>
2022-09-02 11:49:33 +0300

    20220902 1149

---
+++ php/Dynamic/152-maxProduct.php
@@ -0,0 +1,28 @@
+<?php
+
+// 152. Maximum Product Subarray
+
+// Given an integer array nums, 
+// find a contiguous non-empty subarray within the array that has the largest product, 
+// and return the product.
+
+// The test cases are generated so that the answer will fit in a 32-bit integer.
+
+// A subarray is a contiguous subsequence of the array.
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function maxProduct($nums) {
+        
+    }
+}
+
+$nums = [2,3,-2,4];
+
+$solution = new Solution();
+
+print_r($solution->maxProduct( $nums ), false);
