9062a7a950171bf27382bf59f9cc5173d8d7dd0d
kulyaginv <kulyaginv@gmail.com>
2022-09-06 13:12:35 +0300

    20220906 1312

--- php/Stack/227-calculate.php
+++ php/Stack/227-calculate.php
@@ -19,11 +19,51 @@ class Solution {
      * @return Integer
      */
     function calculate($s) {
-        
+        $s = str_replace(' ', '', $s);
+
+        $numbers = [];
+        $operations = [];
+        $stack = [];
+        preg_match_all('!\d+!', $s, $numbers);
+        preg_match_all('[\+|\*|\-|\/]', $s, $operations);
+        $numbers = $numbers[0];
+        $operations = $operations[0];
+        $countOps = count($operations);
+
+        array_push($stack, intval($numbers[0]));
+
+        for($i = 0; $i < $countOps; $i++) {
+            $current = intval($numbers[$i+1]);
+
+            if($operations[$i] == '*') {
+                $current = array_pop($stack) * $current;
+            } 
+
+            if($operations[$i] == '/') {
+
+                $tmp = array_pop($stack);
+                if($tmp > 0) {
+                    $current = floor($tmp / $current);
+                } else {
+                    $current = 0 - floor(abs($tmp) / $current);
+                }
+            }
+            
+            if($operations[$i] == '-') {
+                $current = 0 - $current;
+            }
+
+            array_push($stack, $current);
+        }
+
+        $result = array_sum($stack);
+
+        return $result;
     }
 }
 
-$s = "3+2*2";
+// $s = "3 + 2 * 2 + 7";
+$s = "14-3/2";
 
 $solution = new Solution();
 
