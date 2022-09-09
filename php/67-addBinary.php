41a87a00d307f763b44862f346816cfc23a39f0f
kulyaginv <kulyaginv@gmail.com>
2022-09-09 12:19:00 +0300

    20220909 1218

---
+++ php/67-addBinary.php
@@ -0,0 +1,22 @@
+<?php
+
+//  67. Add Binary
+
+// Given two binary strings a and b, return their sum as a binary string.
+
+class Solution {
+
+    /**
+     * @param String $a
+     * @param String $b
+     * @return String
+     */
+    function addBinary($a, $b) {
+        
+    }
+}
+$a = "11"; $b = "1";
+
+$solution = new Solution();
+
+print_r($solution->addBinary( $a, $b ), false);
