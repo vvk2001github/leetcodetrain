45ce297ba34f5a98fb151c7c8ab19577e841265a
kulyaginv <kulyaginv@gmail.com>
2022-09-13 16:20:54 +0300

    20220913 1620

--- php/BfsDfs/695-maxAreaOfIsland.php
+++ php/BfsDfs/695-maxAreaOfIsland.php
@@ -12,12 +12,46 @@
 
 class Solution {
 
+    private int $m;
+    private int $n;
+    private array $visited;
+    private array $grid;
+    private array $result = [];
+
+    private function foo(int $y, int $x): int {
+        $square = 1;
+        $this->visited[$y][$x] = true;
+
+        if(($y > 0) && (!$this->visited[$y - 1][$x]) && ($this->grid[$y - 1][$x] == 1)) $square = $square + $this->foo($y - 1, $x);
+        if(($y < $this->m - 1) && (!$this->visited[$y + 1][$x]) && ($this->grid[$y + 1][$x] == 1)) $square = $square + $this->foo($y + 1, $x);
+        if(($x > 0) && (!$this->visited[$y][$x - 1]) && ($this->grid[$y][$x - 1] == 1)) $square = $square + $this->foo($y, $x - 1);
+        if(($x < $this->n - 1) && (!$this->visited[$y][$x + 1]) && ($this->grid[$y][$x + 1] == 1)) $square = $square + $this->foo($y, $x + 1);
+
+        if(!in_array($square, $this->result)) $this->result[] = $square;
+
+        return $square;
+    }
+    
+
     /**
      * @param Integer[][] $grid
      * @return Integer
      */
     function maxAreaOfIsland($grid) {
-        
+        $this->m = count($grid);
+        $this->n = count($grid[0]);
+        $this->grid = $grid;
+        $this->visited = array_fill(0, $this->m, []);
+        for($y = 0; $y < $this->m; $y++) $this->visited[$y] = array_fill(0, $this->n, false);
+
+
+        for($y = 0; $y < $this->m; $y++) {
+            for($x = 0; $x < $this->n; $x++) {
+                if(!$this->visited[$y][$x] && $grid[$y][$x] == 1) $this->foo($y, $x);
+            }
+        }
+
+        return max($this->result);
     }
 }
 
@@ -32,4 +66,4 @@ $grid = [   [0,0,1,0,0,0,0,1,0,0,0,0,0],
 
 $solution = new Solution();
 
-print_r( $solution->maxAreaOfIsland($s1, $s2), false);
+print_r( $solution->maxAreaOfIsland($grid), false);
