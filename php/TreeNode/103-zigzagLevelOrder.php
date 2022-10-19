c2bb40d2945abaa247651020079d8e131cc14fab
kulyaginv <kulyaginv@gmail.com>
2022-10-19 12:58:42 +0300

    20221019-1258

--- php/TreeNode/103-zigzagLevelOrder.php
+++ php/TreeNode/103-zigzagLevelOrder.php
@@ -21,23 +21,60 @@ class TreeNode {
 
 class Solution {
 
+    private array $result;
+
+    private function breadth_first(array $nodes = [], int $level = 0): void {
+        $count = count($nodes);
+
+        if($count == 0) return;
+
+        $newNodes = [];
+        $res = [];
+        if($level % 2 == 0) {
+            for($i = 0; $i < $count; $i++) {
+                $res[] = $nodes[$i]->val;
+                if($nodes[$i]->left) $newNodes[] = $nodes[$i]->left;
+                if($nodes[$i]->right) $newNodes[] = $nodes[$i]->right;
+            }
+        } else {
+            for($i = $count - 1; $i >= 0; $i--) {
+                $res[] = $nodes[$i]->val;
+                if($nodes[$count - $i - 1]->left) $newNodes[] = $nodes[$count - $i - 1]->left;
+                if($nodes[$count - $i - 1]->right) $newNodes[] = $nodes[$count - $i - 1]->right;
+            }
+        }
+        
+
+        $this->result[] = $res;
+        $this->breadth_first($newNodes, $level + 1);
+    }
+
     /**
      * @param TreeNode $root
      * @return Integer[][]
      */
     function zigzagLevelOrder($root) {
-        
+
+        if(!$root) return [];
+
+        $this->result = [];
+        $this->breadth_first([$root]);
+        return $this->result;
     }
 }
 
-$node3 = new Node(3);
-$node9 = new Node(9);
-$node20 = new Node(20);
-$node15 = new Node(15);
-$node7 = new Node(7);
+$node3 = new TreeNode(3);
+$node9 = new TreeNode(9);
+$node20 = new TreeNode(20);
+$node15 = new TreeNode(15);
+$node7 = new TreeNode(7);
 
 $node3->left = $node9;
 $node3->right = $node20;
 
 $node20->left = $node15;
 $node20->right = $node7;
+
+$solution = new Solution();
+
+print_r($solution->zigzagLevelOrder( $node3 ), false);
